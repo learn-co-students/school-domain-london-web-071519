68a8c69f647b902086dc91ccfd5fe631e1b1cff6
Harry Chopra <harrychopra@gmail.com>
2019-07-17 17:52:55 +0100

    Completed lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+    attr_accessor :roster, :student, :grade, :school_name
+    
+    def initialize(name)
+        @school_name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @student = name
+        @grade = grade
+        if @roster.key?(@grade)
+            @roster[@grade].push(@student)
+        else
+            @roster[@grade] = [@student]
+        end
+      
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+            sorted = @roster.each{|grades, names| names.sort!}
+            sorted
+    end
+
+end
